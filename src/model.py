3c0bec6bbddb096b76be2b2a52ed3cb5241ebcb8
Charmi Kadi <68164274+charmikadi@users.noreply.github.com>
2025-08-01 18:47:05 -0400

    Update model.py

--- src/model.py
+++ src/model.py
@@ -3,13 +3,6 @@ import torchvision.models as models
 
 def get_resnet_model(num_classes):
     model = models.resnet18(pretrained=True)
-    for param in model.parameters():
-        param.requires_grad = False  # freeze feature extractor
-
-    model.fc = nn.Sequential(
-        nn.Linear(model.fc.in_features, 256),
-        nn.ReLU(),
-        nn.Dropout(0.4),
-        nn.Linear(256, num_classes)
-    )
+    in_features = model.fc.in_features
+    model.fc = nn.Linear(in_features, num_classes)
     return model
