50949cf2dab5f4ea3e39004534c7d00269b0c015
Charmi Kadi <68164274+charmikadi@users.noreply.github.com>
2025-08-01 18:33:55 -0400

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,10 @@
+train_dir = '/content/drive/MyDrive/brain-images/Processed/Training'
+test_dir = '/content/drive/MyDrive/brain-images/Processed/Testing'
+
+batch_size = 32
+epochs = 10
+lr = 0.001
+image_size = (224, 224)
+
+# Save model to drive
+model_save_path = '/content/drive/MyDrive/brain-images/model.pth'
