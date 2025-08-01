ab2d95709411932a2cd206af6b0127e468c5c225
Charmi Kadi <68164274+charmikadi@users.noreply.github.com>
2025-08-01 17:44:47 -0400

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import os
+import torch
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import models
+from sklearn.metrics import classification_report
+
+from google.colab import drive
+drive.mount('/content/drive')
+
+train_dir = '/content/drive/MyDrive/brain-images/Processed/Training'
+test_dir  = '/content/drive/MyDrive/brain-images/Processed/Testing'
+
+# Data transforms  
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.5]*3, [0.5]*3)
+])
+ 
